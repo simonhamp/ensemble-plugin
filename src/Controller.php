289595f971527ca98484bab8c414084906b1483b
Simon Hamp <simon@flipstorm.co.uk>
2018-08-02 01:41:35 +0100

    Split main functionality out into a Controller

---
+++ src/Controller.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace SimonHamp\Ensemble;
+
+use Illuminate\Http\Request;
+use Illuminate\Encryption\Encrypter;
+use Illuminate\Support\Facades\Cache;
+
+class Controller
+{
+    protected $encrypter;
+
+    public function __construct()
+    {
+        AbstractRemoteProcessCall::setCwd(base_path());
+
+        $key = base64_decode(env('ENSEMBLE_PRIVATE_KEY'));
+        $cipher = env('ENSEMBLE_CIPHER', 'AES-256-CBC');
+
+        $this->encrypter = new Encrypter($key, $cipher);
+    }
+
+    public function ensemble(Request $request) {
+        $key = $request->input('key');
+
+        $params = $this->parseParams($key);
+
+        $command = $params->command;
+
+        switch ($command) {
+            case 'outdated':
+                $flags = $params->flags;
+                break;
+
+            default:
+                $flags = 'info';
+        }
+
+        return $this->response($command, $flags);
+    }
+
+    protected function parseParams($key)
+    {
+        return tap(
+            json_decode($this->encrypter->decrypt($key)),
+            function ($payload) {
+                $this->checkPayload($payload);
+            }
+        );
+    }
+
+    protected function checkPayload($payload)
+    {
+        if ($this->hasExpired($payload->expires)) {
+            throw new \Exception('Key has expired');
+        }
+    }
+
+    protected function hasExpired($expires)
+    {
+        $tz = new \DateTimeZone('UTC');
+        $now = new \DateTime('now', $tz);
+        $timeout = new \DateTime($expires, $tz);
+
+        return $now->diff($timeout)->invert;
+    }
+
+    protected function response($command, $flags)
+    {
+        try {
+            $payload = Cache::remember(
+                "ensemble_{$command}_{$flags}",
+                env('ENSEMBLE_CACHE_TTL', 60),
+                function () use ($command, $flags) {
+                    if ($command == 'outdated') {
+                        $flags = explode(',', $flags);
+                    } else {
+                        $flags = [];
+                    }
+
+                    switch ($command) {
+                        case 'outdated':
+                        case 'licenses':
+                            $response = PackageChecker::getJson($command, $flags);
+                            break;
+                    }
+
+                    return $this->encrypter->encrypt($response);
+                }
+            );
+        } catch (\Exception $e) {
+            $payload = $this->errorResponse($e->getMessage(), $command, $flags);
+        }
+
+        return response()->json(['payload' => $payload,]);
+    }
+
+    protected function errorResponse($message, $command, $flags)
+    {
+        return [
+            'failure' => [
+                'reason' => $message,
+                'command' => $command,
+                'flags' => $flags,
+            ],
+        ];
+    }
+}
--- src/EnsembleServiceProvider.php
+++ src/EnsembleServiceProvider.php
@@ -2,27 +2,15 @@
 
 namespace SimonHamp\Ensemble;
 
-use Illuminate\Http\Request;
-use Illuminate\Encryption\Encrypter;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class EnsembleServiceProvider extends ServiceProvider
 {
-    protected $encrypter;
-
     public function register()
     {
         if (! env('ENSEMBLE_ENABLED', false)) {
             return;
         }
-
-        AbstractRemoteProcessCall::setCwd(base_path());
-
-        $key = base64_decode(env('ENSEMBLE_PRIVATE_KEY'));
-        $cipher = env('ENSEMBLE_CIPHER', 'AES-256-CBC');
-
-        $this->encrypter = new Encrypter($key, $cipher);
 
         $this->registerRoutes();
     }
@@ -31,90 +19,7 @@ class EnsembleServiceProvider extends ServiceProvider
     {
         $this->app['router']->post(
             env('ENSEMBLE_ENDPOINT', 'ensemble'),
-            function (Request $request) {
-                $key = $request->input('key');
-
-                $params = $this->parseParams($key);
-
-                $command = $params->command;
-
-                switch ($command) {
-                    case 'outdated':
-                        $flags = $params->flags;
-                        break;
-
-                    default:
-                        $flags = 'info';
-                }
-
-                return $this->response($command, $flags);
-            }
+            '\\SimonHamp\\Ensemble\\Controller@ensemble'
         );
-    }
-
-    protected function parseParams($key)
-    {
-        return tap(
-            json_decode($this->encrypter->decrypt($key)),
-            function ($payload) {
-                $this->checkPayload($payload);
-            }
-        );
-    }
-
-    protected function checkPayload($payload)
-    {
-        if ($this->hasExpired($payload->expires)) {
-            throw new \Exception('Key has expired');
-        }
-    }
-
-    protected function hasExpired($expires)
-    {
-        $tz = new \DateTimeZone('UTC');
-        $now = new \DateTime('now', $tz);
-        $timeout = new \DateTime($expires, $tz);
-
-        return $now->diff($timeout)->invert;
-    }
-
-    protected function response($command, $flags)
-    {
-        try {
-            $payload = Cache::remember(
-                "ensemble_{$command}_{$flags}",
-                env('ENSEMBLE_CACHE_TTL', 60),
-                function () use ($command, $flags) {
-                    if ($command == 'outdated') {
-                        $flags = explode(',', $flags);
-                    } else {
-                        $flags = [];
-                    }
-
-                    if ($command == 'snyk') {
-                        $response = SnykTestRunner::getJson($command);
-                    } else {
-                        $response = PackageChecker::getJson($command, $flags);
-                    }
-
-                    return $this->encrypter->encrypt($response);
-                }
-            );
-        } catch (\Exception $e) {
-            $payload = $this->errorResponse($e->getMessage(), $command, $flags);
-        }
-
-        return response()->json(['payload' => $payload,]);
-    }
-
-    protected function errorResponse($message, $command, $flags)
-    {
-        return [
-            'failure' => [
-                'reason' => $message,
-                'command' => $command,
-                'flags' => $flags,
-            ],
-        ];
     }
 }
